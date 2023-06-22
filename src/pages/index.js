9a159ed86a74228bcfe26849d4d9f77203fa098b
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-22 17:26:34 +0500

    home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,37 @@
+import FeaturedPosts from '../../components/HomePage/FeaturedPosts';
+import Hero from '../../components/HomePage/Hero';
 
+const DUMMY_POSTS = [
+  {
+    title: 'Getting Started with NextJs',
+    slug: 'getting-started-with-nextjs',
+    image: 'getting-started-nextjs.png',
+    date: '2023-02-10',
+    excerpt:
+      "The React Framework for the Web Used by some of the world's largest companies, Next. enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+  },
+  {
+    title: 'Getting Started with NextJs',
+    slug: 'getting-started-with-nextjs2',
+    image: 'getting-started-nextjs.png',
+    date: '2023-02-10',
+    excerpt:
+      "The React Framework for the Web Used by some of the world's largest companies, Next. enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+  },
+  {
+    title: 'Getting Started with NextJs',
+    slug: 'getting-started-with-nextjs3',
+    image: 'getting-started-nextjs.png',
+    date: '2023-02-10',
+    excerpt:
+      "The React Framework for the Web Used by some of the world's largest companies, Next. enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+  },
+];
 export default function Home() {
   return (
     <>
-      Home Page
+      <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
-  )
+  );
 }
