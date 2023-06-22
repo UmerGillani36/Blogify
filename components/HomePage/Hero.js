8f7a5d79ff162f84627303003a8ed974c94fe9cc
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-22 17:23:48 +0500

    hero UI

--- components/HomePage/Hero.js
+++ components/HomePage/Hero.js
@@ -1,8 +1,23 @@
-
+import Image from 'next/image';
+import classes from './Hero.module.css';
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src={'/images/site/umer.jpeg'}
+          alt="An image showing umer"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Umer</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
