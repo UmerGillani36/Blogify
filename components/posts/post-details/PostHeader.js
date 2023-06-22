ec935fd2dceffa2a57e7ad8ce777b0f232ee4506
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-22 17:25:35 +0500

    post header

---
+++ components/posts/post-details/PostHeader.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import classes from './PostHeader.module.css';
+const PostHeader = (props) => {
+  const { title, image } = props;
+  return (
+    <header className={classes.header}>
+      <h1>{title}</h1>
+      <Image src={image} alt={title} width={200} height={150} />
+    </header>
+  );
+};
+
+export default PostHeader;
