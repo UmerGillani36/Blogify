6a360250058dbdcecab492e6716806e54c131fec
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-22 17:23:39 +0500

    featured posts

--- components/HomePage/FeaturedPosts.js
+++ components/HomePage/FeaturedPosts.js
@@ -1,8 +1,12 @@
-
-const FeaturedPosts = () => {
+import PostGrid from '../posts/PostGrid';
+import classes from './FeaturedPosts.module.css';
+const FeaturedPosts = (props) => {
   return (
-    <div>FeaturedPosts</div>
-  )
-}
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostGrid posts={props.posts} />
+    </section>
+  );
+};
 
-export default FeaturedPosts
+export default FeaturedPosts;
