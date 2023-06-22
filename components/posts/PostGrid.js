74419bd849d81eca68319be3ce61771ff9783201
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-22 18:37:09 +0500

    post grid fixes

--- components/posts/PostGrid.js
+++ components/posts/PostGrid.js
@@ -4,7 +4,7 @@ const PostGrid = (props) => {
   const { posts } = props;
   return (
     <ul className={classes.grid}>
-      {posts.map((post) => (
+      {posts?.map((post) => (
         <PostItem key={post.slug} post={post} />
       ))}
     </ul>
