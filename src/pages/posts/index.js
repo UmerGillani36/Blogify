2c2cbcb722390aa1c0e72640ceb6f59bd832da66
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-22 18:26:29 +0500

    with get static props

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,34 +1,21 @@
 import AllPosts from '../../../components/posts/AllPosts';
-const DUMMY_POSTS = [
-  {
-    title: 'Getting Started with NextJs',
-    slug: 'getting-started-with-nextjs',
-    image: 'getting-started-nextjs.png',
-    date: '2023-02-10',
-    excerpt:
-      "The React Framework for the Web Used by some of the world's largest companies, Next. enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
-  },
-  {
-    title: 'Getting Started with NextJs',
-    slug: 'getting-started-with-nextjs2',
-    image: 'getting-started-nextjs.png',
-    date: '2023-02-10',
-    excerpt:
-      "The React Framework for the Web Used by some of the world's largest companies, Next. enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
-  },
-  {
-    title: 'Getting Started with NextJs',
-    slug: 'getting-started-with-nextjs3',
-    image: 'getting-started-nextjs.png',
-    date: '2023-02-10',
-    excerpt:
-      "The React Framework for the Web Used by some of the world's largest companies, Next. enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
-  },
-];
-export default function AllPostPage() {
+import { getFeaturedPosts } from '../../../lib/post-util';
+
+export default function AllPostPage(props) {
+  const { posts } = props;
   return (
     <>
-      <AllPosts posts={DUMMY_POSTS} />
+      <AllPosts posts={posts} />
     </>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 60,
+  };
 }
