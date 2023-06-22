fedcc217eb0713174ce48b02192e2c9181a6720c
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-22 17:24:21 +0500

    main navigation

---
+++ components/Layout/MainNavigation.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Logo from './Logo';
+import classes from './MainNavigation.module.css';
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
