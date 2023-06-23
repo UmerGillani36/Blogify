9265a21ef4a1089742e90b8961495070a962b6b5
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-23 18:46:53 +0500

    contact form UI not completed

---
+++ components/contact/ContactForm.js
@@ -0,0 +1,22 @@
+import classes from './ContactForm.module.css';
+const ContactForm = () => {
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input type="text" id="name" required />
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default ContactForm;
