b9612f07fa33d851151317b756d8a6e7189e1757
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-23 16:26:33 +0500

    with get static props and paths

--- src/pages/posts/[slug]/index.js
+++ src/pages/posts/[slug]/index.js
@@ -1,9 +1,31 @@
 import PostContent from '../../../../components/posts/post-details/PostContent';
+import { getFileNames, getPostData } from '../../../../lib/post-util';
 
-export default function PostDetailPage() {
+export default function PostDetailPage(props) {
+  const { post } = props;
   return (
     <>
-      <PostContent />
+      <PostContent post={post} />
     </>
   );
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const post = getPostData(slug);
+  return {
+    props: {
+      post: post,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const fileNames = getFileNames().map((slug) => slug.replace(/\.md$/, ''));
+  return {
+    paths: fileNames.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 }
