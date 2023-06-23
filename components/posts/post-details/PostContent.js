037808c4e1449d5c941ece69d8fe955662b6ca72
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-23 18:04:19 +0500

    code snippets and theme

--- components/posts/post-details/PostContent.js
+++ components/posts/post-details/PostContent.js
@@ -2,6 +2,9 @@ import PostHeader from './PostHeader';
 import classes from './PostContent.module.css';
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/docco';
 
 const PostContent = (props) => {
   const { post } = props;
@@ -35,6 +38,18 @@ const PostContent = (props) => {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={dark}
+          language={language}
+          children={children}
+        ></SyntaxHighlighter>
+      );
     },
   };
   return (
