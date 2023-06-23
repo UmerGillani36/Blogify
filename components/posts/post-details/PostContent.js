00eee93832b415d023b55eaef910fbc0ed06a4f2
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-23 17:13:51 +0500

    rendring our own img tag in markdown content

--- components/posts/post-details/PostContent.js
+++ components/posts/post-details/PostContent.js
@@ -1,14 +1,46 @@
 import PostHeader from './PostHeader';
 import classes from './PostContent.module.css';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 const PostContent = (props) => {
   const { post } = props;
   const imagePath = `/images/posts/${post.image}`;
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
