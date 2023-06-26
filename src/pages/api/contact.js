ed4cfb9ee4331db48363ab6070f76ebbc602b2b6
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-26 19:01:27 +0500

    adding data into mongoDB

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
     if (
@@ -17,10 +19,27 @@ function handler(req, res) {
       name,
       message,
     };
+    let client;
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://new_user_36:SyKD3QMspWYcd6Pa@cluster0.2hss9zy.mongodb.net/?retryWrites=true&w=majority'
+      );
+    } catch (err) {
+      res.status(500).json({ message: 'Could not connect to database' });
+      return;
+    }
+    try {
+      const db = client.db();
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (err) {
+      res.status(500).json({ message: 'Storing Messages failed' });
+      return;
+    }
+    client.close();
     res
       .status(201)
       .json({ message: 'Successfully stored message', data: newMessage });
-    console.log('message', newMessage);
   }
 }
 
