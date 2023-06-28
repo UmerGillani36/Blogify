c0af8d86d8d9d8b66092cff4073699124cabc0e1
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-28 19:24:24 +0500

    adding head

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import ContactForm from '../../../components/contact/ContactForm';
 
 export default function Contact() {
   return (
     <>
+     <Head>
+      <title>Contact Me</title>
+      <meta name='description' content='Send me your messages.' />
+    </Head>
       <ContactForm />
     </>
   );
