b9cac287a968fa00c0bcda6287539145a22b6f8f
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-28 19:24:21 +0500

    adding head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import FeaturedPosts from '../../components/HomePage/FeaturedPosts';
 import Hero from '../../components/HomePage/Hero';
 import { getFeaturedPosts } from '../../lib/post-util';
-
+import Head from 'next/head';
 export default function Home(props) {
   return (
     <>
+    <Head>
+      <title>Umer' Blog</title>
+      <meta name='description' content='I post about programming and web development.' />
+    </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
