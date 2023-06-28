442b7bdb792c4ea29f540d2d9cdfabce581c0791
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-28 19:23:55 +0500

    adding head in main app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import '@/styles/globals.css';
 import Layout from '../../components/Layout/Layout';
-
+import Head from 'next/head';
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1'></meta>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
