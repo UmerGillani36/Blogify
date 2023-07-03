f6a6186f815d597b1ec6be610302eb2fdd17a269
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-03 12:20:18 +0500

    config and env updation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig;
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+      reactStrictMode: true,
+      env: {
+        mongodb_username: 'new_user_36',
+        mongodb_password: 'SyKD3QMspWYcd6Pa',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: '',
+      },
+    };
+  }
+
+  return {
+    /* config options for all phases except development here */
+    reactStrictMode: true,
+    env: {
+      mongodb_username: 'new_user_36',
+      mongodb_password: 'SyKD3QMspWYcd6Pa',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: '',
+    },
+  };
+};
