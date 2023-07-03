e14dd0cdf1e8744026e15cfbdb4c8c2bf6d1317e
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-03 12:20:46 +0500

    adding env

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -20,10 +20,9 @@ async function handler(req, res) {
       message,
     };
     let client;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.2hss9zy.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
     try {
-      client = await MongoClient.connect(
-        'mongodb+srv://new_user_36:SyKD3QMspWYcd6Pa@cluster0.2hss9zy.mongodb.net/?retryWrites=true&w=majority'
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (err) {
       res.status(500).json({ message: 'Could not connect to database' });
       return;
